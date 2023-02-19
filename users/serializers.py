ffa9564ac96880c6fba2ade469126155ee76812a
alexandrahamd <pylina.alexandra@yandex.ru>
2023-02-19 20:09:11 +0300

    fix user

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 
+from study.models import Payments
 from study.serializers import PaymentsSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    pays = PaymentsSerializer(many=True)
+    pays = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = ['email', 'phone', 'city', 'pays', ]
+
+    def get_pays(self, instanse):
+        return [f'{pay.data_of_payments}, {pay.curs_id}, {pay.summa}$' for pay in Payments.objects.filter(user_id=instanse)]
