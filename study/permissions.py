3d9f7513637c4fc6dd38076421c21f0f2020373c
alexandrahamd <pylina.alexandra@yandex.ru>
2023-02-22 14:16:12 +0300

    fix permissions

--- study/permissions.py
+++ study/permissions.py
@@ -11,8 +11,8 @@ class IsOwner(permissions.BasePermission):
 
 class IsModerator(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.user.has_perms('stydy.update_lesson', 'stydy.update_curs',
+        return request.user.has_perms('stydy.update_lesson', 'stydy.update_curs',
                                   'stydy.view_lesson', 'stydy.view_curs',)
--- study/views.py
+++ study/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from study.models import Curs, Lesson
 from study.permissions import IsOwner, IsModerator
 from study.serializers import CursSerializer, LessonSerializer
@@ -9,6 +11,35 @@ class CursViewSet(viewsets.ModelViewSet):
     queryset = Curs.objects.all()
     serializer_class = CursSerializer
     permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        if request.user.has_perm('study.create_curs') or request.user.is_authenticated:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user.has_perms('stydy.update_curs') or request.user == instance.user:
+            partial = kwargs.pop('partial', False)
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+
+            if getattr(instance, '_prefetched_objects_cache', None):
+                # If 'prefetch_related' has been applied to a queryset, we need to
+                # forcibly invalidate the prefetch cache on the instance.
+                instance._prefetched_objects_cache = {}
+
+            return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user.has_perms('stydy.delete_curs') or request.user == instance.user:
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class LessonListAPIView(generics.ListAPIView):
